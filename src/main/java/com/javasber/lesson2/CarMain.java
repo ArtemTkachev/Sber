e13487a595f0671f27ffb6b6c978deb5dda10dd5
TkachevArtem <art599@yandex.ru>
2021-04-12 16:20:56 +0300

    JavaSber Lesson2 HW Car

---
+++ src/main/java/com/javasber/lesson2/CarMain.java
@@ -0,0 +1,27 @@
+package com.javasber.lesson2;
+
+import com.sun.deploy.security.SelectableSecurityManager;
+
+import java.util.*;
+
+public class CarMain {
+    public static void main(String[] args) {
+        List<Car> listCars =
+                new ArrayList<Car>(Arrays.asList(new Car("Lada", "sedan"),
+                        new Car("Lada", "hatchback"), new Car("Mersedes", "sedan"),
+                        new Car("BMW", "crossover"), new Car("Ford", "hatchback"),
+                        new Car("Peugeot", "crossover"), new Car("Toyota", "sedan")));
+
+        HashMap<String, List<Car>> hashMapCars = new HashMap<>();
+        listCars.forEach((Car car) -> {
+            if (hashMapCars.containsKey(car.getType())) {
+                hashMapCars.get(car.getType()).add(car);
+            } else {
+                List<Car> carType = new ArrayList<Car>();
+                carType.add(car);
+                hashMapCars.put(car.getType(), carType);
+            }
+        });
+        System.out.println("*************");
+    }
+}
